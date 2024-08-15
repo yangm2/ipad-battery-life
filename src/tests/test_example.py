ef1d4d0c1c9930ab9d2264fc6a731a4b586493cc
yangm2 <yangm2@users.noreply.github.com>
2024-08-15 14:54:54 -0700

    add basic tests and mypy

---
+++ src/tests/test_example.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict
+from ..ipados_batteryhealth import ipados_batteryhealth as sut
+import pytest
+
+def test_example():
+    assert True
+
+def test_div0() -> None:
+    tmp: Dict[str, Any] = {
+        "last_value_MaximumCapacityPercent": 999.9,
+        "last_value_MaximumFCC": 0,
+        "last_value_NominalChargeCapacity": 0,
+    }
+
+    cvh = sut.BatteryConfigValueHistogramFinal_V1(**tmp)
+    
+    with pytest.raises(ZeroDivisionError):
+        assert cvh.calc() != 0
