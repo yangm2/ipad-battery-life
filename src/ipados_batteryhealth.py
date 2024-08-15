3714d99b1d192a106992c2d47b589296f45c48be
yangm2 <yangm2@users.noreply.github.com>
2024-08-15 13:15:16 -0700

    add script

---
+++ src/ipados_batteryhealth.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import argparse
+import json
+from dataclasses import dataclass
+from typing import Optional, Dict, Any
+import pathlib
+import pprint
+import re
+
+
+@dataclass(frozen=False)
+class BatteryConfigValueHistogramFinal_V1:
+    last_value_MaximumCapacityPercent: float
+    last_value_MaximumFCC: int  # mAh capacity when new
+    last_value_NominalChargeCapacity: int  # mAh capcity when data acquired
+
+    def __init__(self, **kwargs):
+        if "last_value_MaximumCapacityPercent" in kwargs.keys():
+            self.last_value_MaximumCapacityPercent = kwargs[
+                "last_value_MaximumCapacityPercent"
+            ]
+        if "last_value_MaximumFCC" in kwargs.keys():
+            self.last_value_MaximumFCC = kwargs["last_value_MaximumFCC"]
+        if "last_value_NominalChargeCapacity" in kwargs.keys():
+            self.last_value_NominalChargeCapacity = kwargs[
+                "last_value_NominalChargeCapacity"
+            ]
+
+    def calc(self) -> float:
+        return self.last_value_NominalChargeCapacity / self.last_value_MaximumFCC
+
+    def __repr__(self) -> str:
+        return (
+            f"reported capacity: {self.last_value_MaximumCapacityPercent}%\n"
+            f"calculated capacity: {round(self.calc() * 100, 2)}%"
+        )
+
+
+@dataclass(frozen=False)
+class Item:
+    name: Optional[str] = None
+    message: Optional[Dict[str, Any]] = None
+
+    # pattern for constructing dataclass with subset of data from sloppy invocation
+    def __init__(self, **kwargs):
+        battery_config_re = re.compile(r"BatteryConfigValueHistogramFinal_V\d+")
+
+        if "name" in kwargs.keys():
+            self.name = kwargs["name"]
+
+            if battery_config_re.match(self.name):
+                self.message = BatteryConfigValueHistogramFinal_V1(**kwargs["message"])
+
+
+def main():
+    parser = argparse.ArgumentParser("parse iPad Analytics and print battery health")
+    parser.add_argument(
+        "analytics_filepaths",
+        type=(lambda p: pathlib.Path(p).resolve(strict=True)),
+        nargs="+",
+        help="iPad Analytics input file(s)",
+    )
+    args = parser.parse_args()
+
+    for file in args.analytics_filepaths:
+        print(file)
+
+        with pathlib.Path(file).open(mode="r") as log:
+            for line in log:
+                j = json.loads(line)
+                i = Item(**j)
+
+                if i.message is not None:
+                    pprint.pp(i.message)
+
+
+if __name__ == "__main__":
+    main()
